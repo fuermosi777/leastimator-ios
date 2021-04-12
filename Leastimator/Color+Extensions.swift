9c0217a67d1951f5f8ee491338b8fc68bf5a17da
Hao Liu <liuhao1990@gmail.com>
2021-04-11 23:04:57 -0700

    Add color ext

---
+++ Leastimator/Color+Extensions.swift
@@ -0,0 +1,13 @@
+//
+//  Color+Extensions.swift
+//  Leastimator
+//
+//  Created by Hao Liu on 4/11/21.
+//
+
+import SwiftUI
+
+extension Color {
+  static let mainText = Color("PrimaryText")
+  static let subText = Color("SubText")
+}
